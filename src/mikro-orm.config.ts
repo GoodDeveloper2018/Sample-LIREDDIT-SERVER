be6f429d806bbb7cbaa2a2d3bb60ce850f5f82cb
Arsh <80232792+GoodDeveloper2018@users.noreply.github.com>
2025-01-20 18:54:43 -0500

    Initial project setup
    Initial commit setting up project with TypeScript and MikroORM. Including configuring tsconfig.json to compile TypeScript into the dist directory, adding nodemon for automatic restarts of the compiled JavaScript, defining the Post entity with default date fields using MikroORM’s decorators, and setting up scripts (watch and dev) for a streamlined development workflow.

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,8 @@
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+
+export default {
+        entities: [Post],
+        dbName: 'lireddit',
+        debug: !__prod__,
+    } 
